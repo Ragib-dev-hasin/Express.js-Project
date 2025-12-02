fe8144bc0cd05484f4cc84d66f720a1145df1da9
Ragib-hasin <ragibhasin295@gmail.com>
2025-12-02 17:14:38 +0600

    add app.js code

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("User Management API is running...");
+});
+
+app.use("/api/users", userRoutes);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
